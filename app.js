cd2ac0287e774dadbffb6a6c1b3487760fed240d
Christian Harrypersad <psp1001k2006@gmail.com>
2018-02-07 19:51:23 -0500

    Initial work on the airhorn bot rewrite

---
+++ app.js
@@ -0,0 +1,43 @@
+const Eris = require('eris')
+const sounds = require('./sounds')
+const config = require('./config')
+
+const bot = new Eris(config.discordToken)
+
+bot.on('ready', () => {
+  console.log('Ready')
+  bot.editStatus({name: 'airhorn.wav', type: 2})
+})
+
+bot.on('messageCreate', msg => {
+  const commandArray = msg.content.split(' ')
+  sounds.forEach(sound => {
+    sound.commands.forEach(async command => {
+      if (commandArray[0] === command) {
+        let soundFile = ''
+        if (commandArray[1] !== undefined) {
+          soundFile = `sounds/${sound.prefix}_${commandArray[1]}.dca`
+        } else {
+          soundFile = `sounds/${sound.prefix}_${sound.files[Math.floor(Math.random() * sound.files.length)]}.dca`
+        }
+
+        if (msg.member.voiceState.channelID) {
+          const voiceID = msg.member.voiceState.channelID
+          try {
+            const connection = await bot.joinVoiceChannel(voiceID)
+            console.log(`Playing ${soundFile} in voice channel ${voiceID}`)
+            await connection.play(soundFile, {format: 'dca'})
+            connection.once('end', async () => {
+              await bot.leaveVoiceChannel(voiceID)
+              console.log(`Left voice channel ${voiceID}`)
+            })
+          } catch (e) {
+            console.error(`Error occured in vc ${voiceID}: e.toString()`)
+          }
+        }
+      }
+    })
+  })
+})
+
+bot.connect()
