6fdf251237a372663a464f71a9eb48f10908afee
Christian Harrypersad <psp1001k2006@gmail.com>
2018-02-07 20:12:23 -0500

    All sounds are now exported

--- sounds/index.js
+++ sounds/index.js
@@ -1,3 +1,8 @@
+/* Sound Category File
+ * All the sounds found in this folder have a definition in this file
+ * Any category added has to be added to the array below so it can be seen by the bot
+ */
+
 const airhorn = {
   name: 'Airhorn',
   prefix: 'airhorn',
@@ -6,10 +11,388 @@ const airhorn = {
   ],
   files: [
     'default',
-    'clownfull'
+    'reverb',
+    'spam',
+    'tripletap',
+    'fourtap',
+    'distant',
+    'echo',
+    'clownfull',
+    'clownshort',
+    'clownspam',
+    'highfartlong',
+    'highfartshort',
+    'midshort',
+    'truck'
+  ]
+}
+const overwatch = {
+  name: 'overwatch',
+  prefix: 'owult',
+  commands: [
+    '!overwatch',
+    '!owult'
+  ],
+  files: [
+    // looking for files o[
+    // http://rpboyer15.github.io/files-o[-overwatch/
+    'bastion',
+    'dva_enemy',
+    'dva_friendly',
+    'genji_enemy',
+    'genji_friendly',
+    'hanzo_enemy',
+    'hanzo_friendly',
+    'junkrat_enemy',
+    'junkrat_friendly',
+    'lucio_friendly',
+    'lucio_enemy',
+    'mccree_enemy',
+    'mccree_friendly',
+    'mei_friendly',
+    // //there may be multiple mei friendly ult lines
+    // //from this: https://www.reddit.com/r/Overwatch/comments/4fdw0z/is_that_ultimate_friendly_or_hostile/
+    'mei_enemy',
+    'mercy_friendly',
+    'mercy_friendly_devil',
+    'mercy_friendly_valkyrie',
+    'mercy_enemy',
+    'orisa_enemy',
+    'orisa_friendly',
+    'pharah_enemy',
+    'pharah_friendly',
+    'reaper_enemy',
+    'reaper_friendly',
+    'reinhardt',
+    'roadhog_enemy',
+    'roadhog_friendly',
+    '76_enemy',
+    '76_friendly',
+    'sombra_enemy',
+    'sombra_friendly',
+    'symmetra_teleporter',
+    'symmetra_shield',
+    'torbjorn',
+    'tracer_enemy', // enemy line has constiations. constiations are an argument for splitting it up to be !owtracer, putting them in separate sound collections
+    'tracer_friendly', // doesn't exist?
+    'widow_enemy', // consider shortening to widow?
+    'widow_friendly', // same as above
+    'zarya_enemy',
+    'zarya_friendly',
+    'zenyatta_enemy',
+    'zenyatta_friendly',
+
+    'dva_', // should be in its own sound repository
+    'anyong'
   ]
 }
 
+const khaled = {
+  name: 'khaled',
+  prefix: 'another',
+  commands: [
+    '!anotha',
+    '!anothaone'
+  ],
+  files: [
+    'one',
+    'one_classic',
+    'one_echo'
+  ]
+}
+
+const cena = {
+  name: 'John Cena',
+  prefix: 'jc',
+  commands: [
+    '!johncena',
+    '!cena'
+  ],
+  files: [
+    'airhorn',
+    'echo',
+    'full',
+    'jc',
+    'nameis',
+    'spam'
+  ]
+}
+
+const cow = {
+  name: 'cow',
+  prefix: 'cow',
+  commands: [
+    '!stan',
+    '!stanislav'
+  ],
+  files: [
+    'herd',
+    'moo',
+    'x3'
+  ]
+}
+
+const birthday = {
+  name: 'birthday',
+  prefix: 'birthday',
+  commands: [
+    '!birthday',
+    '!bday'
+  ],
+  files: [
+    'horn',
+    'horn3',
+    'sadhorn',
+    'weakhorn'
+  ]
+}
+
+const roode = {
+  name: 'roode',
+  prefix: 'roode',
+  commands: [
+    '!roode'
+  ],
+  files: [
+    'glorious',
+    'defend',
+    'victorious_full'
+  ]
+}
+
+const revival = {
+  name: 'revival',
+  prefix: 'revival',
+  commands: [
+    '!revival'
+  ],
+  files: [
+    'we_go_hard',
+    'say_yeah'
+  ]
+}
+
+const styles = {
+  name: 'styles',
+  prefix: 'styles',
+  commands: [
+    '!styles',
+    '!aj'
+  ],
+  files: [
+    'gay_community'
+  ]
+}
+
+const dummy = {
+  name: 'dummy',
+  prefix: 'dummy',
+  commands: [
+    '!dummy'
+  ],
+  files: [
+    'yeah'
+  ]
+}
+
+const dota = {
+  name: 'dota',
+  prefix: 'dota',
+  commands: [
+    '!dota',
+    '!tobi',
+    '!tobiwan'
+  ],
+  files: [
+    'alldead',
+    'digitalsports',
+    'dingdingding',
+    'disaster',
+    'liquid',
+    'pudge',
+    'waow'
+  ]
+}
+
+const jones = {
+  name: 'jones',
+  prefix: 'jones',
+  commands: [
+    '!jones',
+    '!alexjones'
+  ],
+  files: [
+    'kissing_goblins',
+    'kissing_goblins_full',
+    'in_bed_goblin',
+    'charging_goblins',
+    'pepsi_taste_test',
+    '1776',
+    'human',
+    'destroy_everything',
+    'hot_blood',
+    'have_children',
+    'gang_of_mustaches',
+    'sick_of_it',
+    'what_is_that_joke',
+    'what_is_venezuela',
+    'the_gay_bomb',
+    'punches',
+    'its_a_gay_bomb',
+    'gay_frogs',
+    'fight_for_your_life'
+  ]
+}
+
+const mummy = {
+  name: 'mummy',
+  prefix: 'mummy',
+  commands: [
+    '!mummy'
+  ],
+  files: [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8'
+  ]
+}
+
+const imhere = {
+  name: 'imhere',
+  prefix: 'imhere',
+  commands: [
+    '!im_here',
+    '!imhere'
+  ],
+  files: [
+    'find_me'
+  ]
+}
+
+const newsreel = {
+  name: 'newsreel',
+  prefix: 'newsreel',
+  commands: [
+    '!newsreel'
+  ],
+  files: [
+    'ooh_swish'
+  ]
+}
+
+const logan = {
+  name: 'logan',
+  prefix: 'logan',
+  commands: [
+    '!logan'
+  ],
+  files: [
+    '1',
+    '2',
+    '3',
+    '4'
+  ]
+}
+
+const foxy = {
+  name: 'foxy',
+  prefix: 'foxy',
+  commands: [
+    '!foxy'
+  ],
+  files: [
+    'trying'
+  ]
+}
+
+const money = {
+  name: 'money',
+  prefix: 'money',
+  commands: [
+    '!money'
+  ],
+  files: [
+    'lodsofemone',
+    'lodsofemone_full',
+    'wopitout'
+  ]
+}
+
+const gw2 = {
+  name: 'gw2',
+  prefix: 'gw2',
+  commands: [
+    '!gw2'
+  ],
+  files: [
+    'rose'
+  ]
+}
+
+const weed = {
+  name: 'weed',
+  prefix: 'theweed',
+  commands: [
+    '!theweed',
+    '!weed'
+  ],
+  files: [
+    'all'
+  ]
+}
+
+const tf2 = {
+  name: 'tf2',
+  prefix: 'tf2',
+  commands: [
+    '!tf2'
+  ],
+  files: [
+    'overtime1',
+    'overtime2',
+    'overtime3',
+    'overtime4'
+  ]
+}
+
+const assblast = {
+  name: 'assblast',
+  prefix: 'assblastusa',
+  commands: [
+    '!assblastusa'
+  ],
+  files: [
+    'full'
+  ]
+}
+
+// This module exports all the sound catgories used in the bot
 module.exports = [
-  airhorn
+  airhorn,
+  overwatch,
+  khaled,
+  cena,
+  cow,
+  birthday,
+  roode,
+  revival,
+  styles,
+  dummy,
+  dota,
+  jones,
+  mummy,
+  imhere,
+  newsreel,
+  logan,
+  foxy,
+  money,
+  gw2,
+  weed,
+  tf2,
+  assblast
 ]
